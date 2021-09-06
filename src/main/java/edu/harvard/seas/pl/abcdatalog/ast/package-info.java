bd22b118addc807ea6757082349281d2bbf1e9c2
Aaron Bembenek <aaron.bembenek@gmail.com>
2021-09-06 14:44:27 -0400

    Fix messed up package-info.java file.

--- src/main/java/edu/harvard/seas/pl/abcdatalog/ast/package-info.java
+++ src/main/java/edu/harvard/seas/pl/abcdatalog/ast/package-info.java
@@ -1,6 +1,9 @@
 /**
  * This packages contains classes representing the core abstract syntax tree for
- * AbcDatalog. Many of these classes support the visitor design pattern;
+ * AbcDatalog. Many of these classes support the visitor design pattern; see
+ * {@link edu.harvard.seas.pl.abcdatalog.ast.visitors}.
+ */
+package edu.harvard.seas.pl.abcdatalog.ast;
 
 /*-
  * #%L
@@ -34,7 +37,3 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  * #L%
  */
-see
- * {@link edu.harvard.seas.pl.abcdatalog.ast.visitors}.
- */
-package edu.harvard.seas.pl.abcdatalog.ast;
