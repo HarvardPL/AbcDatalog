d6b60a97417393c0c9bfaac2e269ff0f5fc916dc
Aaron Bembenek <aaron.bembenek@gmail.com>
2023-12-27 15:53:32 -0500

    v0.7.0 (#21)
    * Script to partially automate release.
    
    * Bump version.

---
+++ make_release.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# This script is used to partially automate the release process. You still need
+# to update the version number in `pom.xml`.
+
+set -e
+
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$script_dir"
+
+git checkout master
+# Make sure code builds
+mvn package
+# Make sure all files have licenses
+mvn license:update-file-header
+# Make sure code is formatted consistently
+mvn com.spotify.fmt:fmt-maven-plugin:format
+# Generate Javadocs
+mvn javadoc:javadoc
+git add src
+git commit -allow-empty -m "Release commit."
+
+# Update the Javadocs on the website
+git checkout gh-pages
+rm -rf apidocs
+cp -r target/site/apidocs .
+git add apidocs
+git commit –allow-empty -m "Update Javadocs."
+git push
+git checkout master
