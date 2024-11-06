4e25a86dc7f346e6056eaa8baee38b96a261bc88
Ulrik Rasmussen <ulrik@utr.dk>
2024-11-06 21:12:51 +0100

    Fix bug in bound variables check; add regression tests (#25)
    * Fix bug in bound variables check; add regression tests
    
    * Expand tests; reformat.
    
    ---------
    
    Co-authored-by: Ulrik Rasmussen <ulrik.rasmussen@deondigital.com>
    Co-authored-by: Aaron Bembenek <aaron.bembenek@gmail.com>

--- src/test/java/edu/harvard/seas/pl/abcdatalog/engine/ExplicitUnificationTests.java
+++ src/test/java/edu/harvard/seas/pl/abcdatalog/engine/ExplicitUnificationTests.java
@@ -150,4 +150,14 @@ public abstract class ExplicitUnificationTests extends AbstractTests {
     test("p(X) :- q(X), Y!=_. q(a).", "p(X)?", "");
     throw new DatalogValidationException();
   }
+
+  @Test
+  public void testVariablesBoundByUnifiers1() throws DatalogValidationException {
+    test("p(X, Y) :- q(Z, W), X = Z, W = Y. q(a, b).", "p(a, b)?", "p(a, b).");
+  }
+
+  @Test
+  public void testVariablesBoundByUnifiers2() throws DatalogValidationException {
+    test("p(X, Y) :- q(Z, W), Z = A, B = W, X = A, B = Y. q(a, b).", "p(a, b)?", "p(a, b).");
+  }
 }
